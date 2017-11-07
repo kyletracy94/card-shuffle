7769f7a8db54d262d1324d093e5b53eac5809968
Tracy, Kyle J. <KTracy@tmamission.com>
2017-11-07 15:19:02 -0500

    SHU-1
    Made changes to card shuffle tests

--- src/main/java/card/shuffle/classes/Deck.java
+++ src/main/java/card/shuffle/classes/Deck.java
@@ -9,7 +9,7 @@ import java.util.Random;
 
 
 /**
- * This object is a representation of a Deck. Holds up to 52 Cards
+ * This object is a representation of a Deck
  * 
  * @author kyle
  *
@@ -96,5 +96,30 @@ public class Deck implements CardOperations {
 		}
 
 	}
-	
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cards == null) ? 0 : cards.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Deck other = (Deck) obj;
+        if (cards == null) {
+            if (other.cards != null)
+                return false;
+        } else if (!cards.equals(other.cards))
+            return false;
+        return true;
+    }
+
 }
